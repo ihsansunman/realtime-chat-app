959f2e80f9aaf2d5e548d2249010616163e6a67e
ihsansunman <ihsasunman@yandex.com>
2022-06-03 11:42:16 +0300

    Connect BackEnd

--- src/Components/Container.js
+++ src/Components/Container.js
@@ -1,8 +1,12 @@
-import React from "react";
-import ChatList from "./ChatList"
-import ChatForm from "./ChatForm"
+import { useEffect } from "react";
+import ChatList from "./ChatList";
+import ChatForm from "./ChatForm";
+import { init } from "../Api/socketApi";
 
 export default function Container() {
+  useEffect(() => {
+    init();
+  }, []);
   return (
     <div className="App">
       <ChatList />
