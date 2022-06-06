704e4d564e1197350bccf92b26285986d7574515
ihsansunman <ihsasunman@yandex.com>
2022-06-06 09:23:54 +0300

    Set Send Messages

--- src/Api/socketApi.js
+++ src/Api/socketApi.js
@@ -1,9 +1,13 @@
 import io from "socket.io-client";
 let socket;
 export const init = () => {
-    console.log("Connecting...")
+  console.log("Connecting...");
   socket = io("http://localhost:3000", {
     transports: ["websocket"],
   });
-  socket.on("connect", ()=>console.log("connected!"))
+  socket.on("connect", () => console.log("connected!"));
 };
+
+export const sendMessage =(message)=>{
+  if (socket) socket.emit("new-message", message)
+}
--- src/Components/ChatForm.js
+++ src/Components/ChatForm.js
@@ -1,12 +1,21 @@
 import { useState } from "react";
 import styles from "./styles.module.css";
+import { sendMessage } from "../Api/socketApi";
+import { useChat } from "../Context/ChatContext";
 
 export default function ChatForm() {
-  const [message, setMessage] = useState("[]");
-  const handleSubmit=(e)=>{
-    e.preventDefault()
+  const [message, setMessage] = useState("");
+  const { setMessages } = useChat();
 
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setMessages((prevState)=>[...prevState, {message}])
+
+    sendMessage(message);
+
+    setMessage("");
+  };
 
   return (
     <div>
